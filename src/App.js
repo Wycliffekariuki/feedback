904a338cb51999f528da7dc8b802fed11c8f8230
Wycliffe kariuki <cliffinkaemurd@gmail.com>
2025-02-12 09:57:30 +0300

    This is the first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { useForm } from 'react-hook-form';
+import Feedback from './components/Feedback';
+import './App.css';
+
+function App() {
+  return (
+    <Feedback />
+    
+  );
+}
+
+export default App;
